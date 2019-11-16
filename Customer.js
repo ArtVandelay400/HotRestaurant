70832a01edc8d9e2a1f2fb34b0b965994d595104
Levi Giraud <giraudlevi@gmail.com>
2019-11-16 10:26:22 -0700

    created basic Customer class

---
+++ Customer.js
@@ -0,0 +1,12 @@
+class Customer {
+    
+    constructor(customerName, phoneNumber, customerEmail, customerID) {
+        this.customerName = customerName;
+        this.phoneNumber = phoneNumber;
+        this.customerEmail = customerEmail;
+        this.customerID = customerID;
+    }
+    
+}
+
+module.exports = Customer;
